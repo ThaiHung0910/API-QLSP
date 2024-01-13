bf40689540471fdc69628adfdacae8ffdc1ad0bf
ThaiHung <thaihung147741@gmail.com>
2024-01-13 21:23:49 +0700

    commit

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -11,33 +11,66 @@ Các method của axios - thường có 4 (default, có thể khác nếu backen
 
 // Function
 
-var tenSp = document.getElementById('TenSP')
-var giaSp = document.getElementById('GiaSP')
-var hinhSp = document.getElementById('HinhSP')
-var moTaSp = document.getElementById('MoTaSP')
+var tenSp = document.getElementById("TenSP");
+var giaSp = document.getElementById("GiaSP");
+var hinhSp = document.getElementById("HinhSP");
+var moTaSp = document.getElementById("MoTaSP");
+var btnAddProduct = document.getElementById("btnAddProduct");
+var btnUpdateProduct = document.getElementById("btnUpdateProduct");
+var inputForm = document.querySelectorAll(".form-group input");
 // Tao global var de luu id cho method update
-var idEdited = null
-
+var idEdited = null;
 
 function turnOnLoading() {
-  document.getElementById('spinner').style.display = 'block'
+  document.getElementById("spinner").style.display = "block";
 }
 
 function turnOffLoading() {
-  document.getElementById('spinner').style.display = 'none'
+  document.getElementById("spinner").style.display = "none";
 }
 
 function resetForm() {
-  var inputForm = document.querySelectorAll('input')
-  for(var i = 0; i < inputForm.length; i++) {
-    inputForm[i].value = ''
+  for (var i = 0; i < inputForm.length; i++) {
+    inputForm[i].value = "";
+  }
+}
+
+function getValueProduct() {
+  var errorInput = document.querySelectorAll("span.sp-thongbao");
+  var isValid = true;
+  for (var i = 0; i < inputForm.length; i++) {
+    var inputId = inputForm[i].id;
+    var errorId = errorInput[i].id;
+    var inputValue = inputForm[i].value;
+    switch (inputId) {
+      case "GiaSP":
+        isValid &=
+          checkEmptyValue(inputValue, errorId) &&
+          checkNumber(inputValue, errorId);
+        break;
+      case "HinhSP":
+        isValid &=
+          checkEmptyValue(inputValue, errorId) &&
+          checkImage(inputValue, errorId);
+        break;
+      default:
+        isValid &= checkEmptyValue(inputValue, errorId);
+    }
+  }
+  if (isValid) {
+    return {
+      name: tenSp.value,
+      price: giaSp.value,
+      img: hinhSp.value,
+      desc: moTaSp.value,
+    };
   }
 }
 
 function renderProducts(productArray) {
   var content = "";
   // for (var i = 0; i < productArray.length; i++)
-  for (var i = productArray.length - 1; i >= 0 ; i--) {
+  for (var i = productArray.length - 1; i >= 0; i--) {
     var product = productArray[i];
     content += `
                 <tr>
@@ -45,7 +78,7 @@ function renderProducts(productArray) {
                   <td>${product.name}</td>
                   <td>${product.price}</td>
                   <td>
-                      <img src="${product.img}" alt="">
+                      <img class="img" src="${product.img}" alt="Image not found">
                   </td>
                   <td>${product.desc}</td>
                   <td>
@@ -60,113 +93,113 @@ function renderProducts(productArray) {
 }
 
 function fetchProductsList() {
-    turnOnLoading()
-    axios({
-        url:'https://6597f7c2668d248edf23d04d.mockapi.io/product',
-        method: "GET",
-      })
-        .then(function (res) {
-          renderProducts(res.data);
-          console.log("RES: ", res.data);
-          turnOffLoading()
-        })
-        .catch(function (err) {
-          console.log("ERR: ", err);
-          turnOffLoading()
-        });
-}
-fetchProductsList()
-
-function deleteProduct(id) {
-  turnOnLoading()
+  turnOnLoading();
   axios({
-    url:`https://6597f7c2668d248edf23d04d.mockapi.io/product/${id}`,
-    method: "DELETE",
+    url: "https://6597f7c2668d248edf23d04d.mockapi.io/product",
+    method: "GET",
   })
     .then(function (res) {
-      fetchProductsList()
+      renderProducts(res.data);
       console.log("RES: ", res.data);
+      turnOffLoading();
     })
     .catch(function (err) {
-      turnOffLoading()
       console.log("ERR: ", err);
+      turnOffLoading();
     });
+}
+fetchProductsList();
+
+function deleteProduct(id) {
+  if (confirm("Are you sure you want to delete this product?")) {
+    turnOnLoading();
+    axios({
+      url: `https://6597f7c2668d248edf23d04d.mockapi.io/product/${id}`,
+      method: "DELETE",
+    })
+      .then(function (res) {
+        fetchProductsList();
+        console.log("RES: ", res.data);
+      })
+      .catch(function (err) {
+        turnOffLoading();
+        console.log("ERR: ", err);
+      });
+  }
 }
 
 function createProduct() {
-  
-  turnOnLoading()
-  
-  var product = {
-    name: tenSp.value,
-    price: giaSp.value,
-    img: hinhSp.value,
-    desc: moTaSp.value
-  }
+  var product = getValueProduct();
 
-  axios({
-    url: "https://6597f7c2668d248edf23d04d.mockapi.io/product",
-    method: "POST",
-    // thêm method data
-    data: product
-  })
-    .then(function (res) {
-      fetchProductsList()
-      // Tat modal sau khi them thanh cong
-      $('#myModal').modal('hide')
-      console.log("RES: ", res.data);
-      resetForm()
+  if (product) {
+    turnOnLoading();
+    axios({
+      url: "https://6597f7c2668d248edf23d04d.mockapi.io/product",
+      method: "POST",
+      // thêm method data
+      data: product,
     })
-    .catch(function (err) {
-      turnOffLoading()
-      console.log("ERR: ", err);
-    });
+      .then(function (res) {
+        fetchProductsList();
+        // Tat modal sau khi them thanh cong
+        $("#myModal").modal("hide");
+        console.log("RES: ", res.data);
+        resetForm();
+      })
+      .catch(function (err) {
+        turnOffLoading();
+        console.log("ERR: ", err);
+      });
+  }
 }
 
-
 function editProduct(id) {
-  idEdited = id
+  btnAddProduct.style.display = "none";
+  btnUpdateProduct.style.display = "block";
+  idEdited = id;
   axios({
     url: `https://6597f7c2668d248edf23d04d.mockapi.io/product/${id}`,
     method: "GET",
-  }) 
-    .then(function (res) {
-      var product = res.data
-      console.log("RES: ", res.data);
-      tenSp.value = product.name
-      giaSp.value = product.price
-      hinhSp.value = product.img
-      moTaSp.value = product.desc
-      $('#myModal').modal('show')
-    })
-    .catch(function (err) {
-      console.log("ERR: ", err);
-    })
-}
-
-function updateProduct() {
-  turnOnLoading()
-  var product = {
-    name: tenSp.value,
-    price: giaSp.value,
-    img: hinhSp.value,
-    desc: moTaSp.value
-  }
-  
-  axios({
-    url: `https://6597f7c2668d248edf23d04d.mockapi.io/product/${idEdited}`,
-    method: "PUT",
-    data: product
   })
     .then(function (res) {
-      fetchProductsList()
-      // // Tat modal sau khi them thanh cong
-      $('#myModal').modal('hide')
+      var product = res.data;
       console.log("RES: ", res.data);
+      tenSp.value = product.name;
+      giaSp.value = product.price;
+      hinhSp.value = product.img;
+      moTaSp.value = product.desc;
+      $("#myModal").modal("show");
     })
     .catch(function (err) {
-      turnOffLoading()
       console.log("ERR: ", err);
     });
 }
 
+function updateProduct() {
+  var product = getValueProduct();
+
+  if (product) {
+    turnOnLoading();
+    axios({
+      url: `https://6597f7c2668d248edf23d04d.mockapi.io/product/${idEdited}`,
+      method: "PUT",
+      data: product,
+    })
+      .then(function (res) {
+        fetchProductsList();
+        // // Tat modal sau khi them thanh cong
+        $("#myModal").modal("hide");
+        console.log("RES: ", res.data);
+      })
+      .catch(function (err) {
+        turnOffLoading();
+        console.log("ERR: ", err);
+      });
+  }
+}
+
+document.getElementById("btnThemSP").addEventListener("click", function () {
+  resetForm();
+  btnAddProduct.style.display = "block";
+  btnUpdateProduct.style.display = "none";
+});
---
+++ assets/js/vadation.js
@@ -0,0 +1,45 @@
+function getElement(element) {
+  return document.getElementById(element);
+}
+
+var message = ["Vui lòng không bỏ trống", "Vui lòng chỉ nhập số", "Vui lòng nhập đúng định dạng hình ảnh"];
+
+function checkEmptyValue(value, errorId) {
+  if (value) {
+    getElement(errorId).style.display = "none";
+    getElement(errorId).innerHTML = "";
+    return true;
+  } else {
+    getElement(errorId).style.display = "block";
+    getElement(errorId).innerHTML = message[0];
+    return false;
+  }
+}
+
+function checkNumber(value, errorId) {
+  var regexValue = /^-{0,1}\d*\.{0,1}\d+$/
+  if(regexValue.test(value)) {
+    getElement(errorId).style.display = 'none'
+     getElement(errorId).innerHTML = ''
+    return true
+  } else {
+    getElement(errorId).style.display = 'block'
+    getElement(errorId).innerHTML = message[1]
+    return false
+  }
+}
+
+function checkImage(value, errorId) {
+    var res = value.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    if(res !== null) {
+      getElement(errorId).style.display = 'none'
+       getElement(errorId).innerHTML = ''
+      return true
+    } else {
+      getElement(errorId).style.display = 'block'
+      getElement(errorId).innerHTML = message[2]
+      return false
+    }
+}
+
+
